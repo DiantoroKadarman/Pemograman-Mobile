24d93518ccd6bcad62da6b5b8f91a18fb9cd434e
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-09-16 23:45:09 +0700

    tugas

--- Tugas.dart
+++ Tugas.dart
@@ -1,13 +1,14 @@
 void main(List<String> args) {
-  for (int i = 2; i < 201; i++) {
+  for (int i = 2; i < 202; i++) {
     int j = 2;
-    for (; j <= i ~/ 2; j++) {
+    for (; j <= i / 2; j++) {
       if (i % j == 0) {
         break;
       }
     }
-    if (j > i ~/ 2) {
+    if (j > i / 2) {
       print(i);
+
     }
   }
 }
