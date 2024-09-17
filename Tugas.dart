6ea9c6340c8a0fa91ded3c31cfcc44f82343be05
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-09-18 00:36:43 +0700

    tugas

--- Tugas.dart
+++ Tugas.dart
@@ -1,4 +1,5 @@
 void main(List<String> args) {
+  bool check = false;
   for (int i = 2; i < 202; i++) {
     int j = 2;
     for (; j <= i / 2; j++) {
@@ -8,7 +9,10 @@ void main(List<String> args) {
     }
     if (j > i / 2) {
       print(i);
-
+      check = true;
     }
   }
+  if (check == true) {
+      print("Nama\t: Diantoro Kadarman \nNIM\t: 2241720084");
+    }
 }
