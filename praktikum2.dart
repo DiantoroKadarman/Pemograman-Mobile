d404437bcec990a09df2b1f852633cfdd9c10fa5
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-09-28 18:05:22 +0700

    praktikum2

--- praktikum2.dart
+++ praktikum2.dart
@@ -1,5 +1,19 @@
 void main(List<String> args) {
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-  print(halogens);
-  print(halogens[0]);
+  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  // print(halogens);
+
+  var names1 = <String>{};
+  Set<String> names2 = {}; // This works, too.
+  var names3 = {}; // Creates a map, not a set.
+
+  // Menambahkan nama dan NIM menggunakan .add()
+  names1.add('Diantoro');
+  names1.add('2241720084');
+
+  // Menambahkan nama dan NIM menggunakan .addAll()
+  names2.addAll({'Diantoro', '2241720084'});
+
+  print(names1);
+  print(names2);
+
 }
