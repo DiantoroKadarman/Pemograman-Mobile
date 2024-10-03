3fca3d26e13ec2cf3068e3c28546fbdf8f6b0c84
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-10-03 10:41:07 +0700

    update praktikum 3

--- praktikum3.dart
+++ praktikum3.dart
@@ -1,4 +1,4 @@
-void main(List<String> args) {
+void main() {
   var gifts = {
     // Key:    Value
     'first': 'partridge',
@@ -25,7 +25,23 @@ void main(List<String> args) {
   nobleGases[10] = 'neon';
   nobleGases[18] = 'argon';
 
-  print(mhs1);
-  print(mhs2);
-  
+
+  // menambahkan elemen nama dan NIM
+  gifts['nama'] = 'Diantoro Kadarman';
+  gifts['NIM'] = '2241720084';
+
+  nobleGases[11] = 'Diantoro Kadarman';
+  nobleGases[12] = '2241720084';
+
+  mhs1['NAMA'] = 'Diantoro Kadarman';
+  mhs1['NIM'] = '2241720084';
+
+  mhs2[1] = 'Diantoro Kadarman';
+  mhs2[2] = '2241720084';
+
+  print('\nMenambahkan elemen Nama dan NIM');
+  print('variable gifts $gifts');
+  print('variable noble $nobleGases');
+  print('variable mhs1 $mhs1');
+  print('variable mhs2 $mhs2');
 }
