0d1235c11fe306a59cd2a2beacd79f4f685b650b
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-10-05 01:18:27 +0700

    Update praktikum 5

--- praktikum5.dart
+++ praktikum5.dart
@@ -1,17 +1,17 @@
 void main(List<String> args) {
-  // var record = ('first', a: 2, b: true, 'last');
-  // print(record);
+  var record = ('first', a: 2, b: true, 'last');
+  print(record);
 
-  // (int, int) tukar((int, int) record) {
-  //   var (a, b) = record;
-  //   return (b, a);
-  // }
-  // var result = tukar((5, 10));
-  // print(result);
+  (int, int) tukar((int, int) record) {
+    var (a, b) = record;
+    return (b, a);
+  }
+  var result = tukar((5, 10));
+  print(result);
 
-  // // Record type annotation in a variable declaration:
-  // (String, int) mahasiswa = ('Diantoro Kadarman', 2241720084);
-  // print(mahasiswa);
+  // Record type annotation in a variable declaration:
+  (String, int) mahasiswa = ('Diantoro Kadarman', 2241720084);
+  print(mahasiswa);
 
   var mahasiswa2 = ('Diantoro Kadarman', a: 2241720084, b: true, 'last');
 
@@ -19,5 +19,5 @@ void main(List<String> args) {
   print(mahasiswa2.a); // Prints NIM
   print(mahasiswa2.b); // Prints true
   print(mahasiswa2.$2); // Prints 'last'
-  
+
 }
