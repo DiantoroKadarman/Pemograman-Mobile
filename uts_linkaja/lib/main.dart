384164f7e21c77816c9a7239fdd1cc71f007d03c
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-10-27 04:40:43 +0700

    UTS

--- uts_linkaja/lib/main.dart
+++ uts_linkaja/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Diantoro_2241720084',
+      title: 'UTS_Diantoro_2241720084',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
