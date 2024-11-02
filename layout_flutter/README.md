599439170dae0450256fe49840798a3a76a623af
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-11-03 04:06:41 +0700

    Praktikum 1-4

--- layout_flutter/README.md
+++ layout_flutter/README.md
@@ -2,12 +2,37 @@
 
 A new Flutter project.
 
-### Praktikum 1: Membangun Layout di Flutter
-## Langkah 1 & 2
+## Praktikum 1: Membangun Layout di Flutter
+### Langkah 1 & 2
 ![alt text](image.png)
 
-## Langkah 3: Identifikasi layout diagram
-## Langkah 4: Implementasi title row
+### Langkah 3: Identifikasi layout diagram
+### Langkah 4: Implementasi title row
+
+/* soal 1 */ Letakkan widget Column di dalam widget Expanded agar menyesuaikan ruang yang tersisa di dalam widget Row. Tambahkan properti crossAxisAlignment ke CrossAxisAlignment.start sehingga posisi kolom berada di awal baris.
+
+/* soal 2 */ Letakkan baris pertama teks di dalam Container sehingga memungkinkan Anda untuk menambahkan padding = 8. Teks ‘Batu, Malang, Indonesia' di dalam Column, set warna menjadi abu-abu.
+
+/* soal 3 */ Dua item terakhir di baris judul adalah ikon bintang, set dengan warna merah, dan teks "41". Seluruh baris ada di dalam Container dan beri padding di sepanjang setiap tepinya sebesar 32 piksel. Kemudian ganti isi body text ‘Hello World' dengan variabel titleSection seperti berikut:
+
 ![alt text](image-1.png)
+
+## Praktikum 2: Implementasi button row
+### Langkah 1 & 2 : Buat widget buttonSection
+
+![alt text](image-2.png)
+
+### Langkah 3 : Tambah button section ke body
+
+![alt text](image-3.png)
+
+## Praktikum 3: Implementasi text section
+### Langkah 1: Buat widget textSection
+
+
+### Langkah 2: Tambahkan variabel text section ke body
+
+
+
 
 
--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -5,6 +5,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: const Text(
+      'Coban Rondo memiliki ketinggian sekitar 84 meter. Sedangkan, lokasi air terjun berada pada ketinggian 1.134 meter di atas permukaan laut. Udara di sekitar sejuk dan tempat yang sesuai untuk liburan. Pada musim penghujan, debit air terjun dapat mencapai 150 liter per detik,  sedangkan pada musim kemarau dabit air terjun sekitar 90 liter meter per detik. Dengan air terjun yang cukup tinggi, pengunjung yang mendekati air terjun akan terkena titik-titik air yang diterbangkan angin. Semakin dekat air terjun, pakaian akan semakin basah. Selain melihat panorama air terjun yang turun dari tebing, pengelola juga menyediakan sejumlah fasilitas wisata, seperti camping ground, tempat pemancingan, jogging track, penginapan, lintasan sepeda gunung, mengitari labirin, maupun persemian tanaman hutan. '
+      'Diantoro Kadarman - 2241720084',
+      softWrap: true,
+    ),
+  );
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   final Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
@@ -46,6 +76,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Diantoro Kadarman / 2241720084',
       home: Scaffold(
@@ -55,6 +96,8 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
